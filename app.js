fa398b6a4f4c4a6c431b69b5fcfa7517e89234bf
Saad Zafar <saadie.111@gmail.com>
2018-02-23 21:25:12 +0500

    express server and handlebars view engine setup

---
+++ app.js
@@ -0,0 +1,20 @@
+const path = require('path');
+const express = require('express');
+const expresshbs = require('express-handlebars');
+
+const app = express();
+
+
+app.use(express.static(path.join(__dirname,'public')));
+
+app.engine('handlebars',expresshbs({defaultLayout:'home'}));
+app.set('view engine','handlebars');
+
+app.get('/',(req,res)=>{
+    res.render('home/index');
+});
+
+const port = process.env.PORT || 5000;
+app.listen(port,()=>{
+    console.log(`App started on port ${port}`);
+});
