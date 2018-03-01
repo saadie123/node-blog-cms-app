35e041cdf09d464e9b040a3dc586d7678ad8ee2b
Saad Zafar <saadie.111@gmail.com>
2018-03-01 18:20:02 +0500

    Added login and register views

--- app.js
+++ app.js
@@ -18,6 +18,14 @@ app.get('/about',(req,res)=>{
     res.render('home/about');
 });
 
+app.get('/register',(req,res)=>{
+    res.render('home/register');
+})
+
+app.get('/login',(req,res)=>{
+    res.render('home/login');
+})
+
 const port = process.env.PORT || 5000;
 app.listen(port,()=>{
     console.log(`App started on port ${port}`);
