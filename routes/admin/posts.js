4db95ce8209ce35774476bcefbddebfe64f0f749
Saad Zafar <31594740+saadie123@users.noreply.github.com>
2018-03-02 15:12:32 +0500

    Refactored post routes by using async/await

--- routes/admin/posts.js
+++ routes/admin/posts.js
@@ -8,65 +8,65 @@ router.all('/*',(req,res,next)=>{
     next();
 });
 
-router.get('/',(req,res)=>{
-    Post.find().then(posts=>{
+router.get('/',async (req,res)=>{
+    try{
+        let posts = await Post.find();
         res.render('admin/posts/index',{posts});
-    }).catch(error=>{
-        console.log(error);
-    })
+    } catch(error){
+        console.log(error);        
+    }
 });
 
 router.get('/create',(req,res)=>{
     res.render('admin/posts/create');
 });
 
-router.get('/edit/:id',(req,res)=>{
-    Post.findById(req.params.id).then(post=>{
-        res.render('admin/posts/edit',{post});
-    })
+router.get('/edit/:id',async (req,res)=>{
+    try{
+        let post = await Post.findById(req.params.id);
+        res.render('admin/posts/edit',{post});        
+    } catch(error){
+        console.log(error);
+    }
 })
 
-router.post('/create',(req,res)=>{
-    let allowComments = req.body.allowComments === 'on' ? true : false;
-    let post = new Post({
-        title: req.body.title,
-        status: req.body.status,
-        description: req.body.description,
-        allowComments
-    });
-    post.save()
-    .then(post=>{
-        console.log(post);
-    })
-    .catch(error=>{
+router.post('/create',async (req,res)=>{
+    try{
+        let allowComments = req.body.allowComments === 'on' ? true : false;
+        let post = new Post({
+            title: req.body.title,
+            status: req.body.status,
+            description: req.body.description,
+            allowComments
+        });
+        let newPost = await post.save();
+        res.redirect('/admin/posts');
+    } catch(error){
         console.log(error);
-    });
-    res.redirect('/admin/posts');
+    }
 });
 
-router.put('/edit/:id',(req,res)=>{
+router.put('/edit/:id',async (req,res)=>{
     let allowComments = req.body.allowComments === 'on' ? true : false;
     let body = {
         ...req.body,
         allowComments
     }
-    Post.findByIdAndUpdate(req.params.id,{$set:body})
-    .then(post=>{
-        res.redirect('/admin/posts');
-    })
-    .catch(error=>{
-       console.log(error);
-    });
+    try {
+        let post = await Post.findByIdAndUpdate(req.params.id,{$set:body},{new:true});
+        res.redirect('/admin/posts');   
+    } catch (error) {
+        console.log(error)
+    }    
 });
 
-router.delete('/:id',(req,res)=>{
-    Post.findByIdAndRemove(req.params.id)
-    .then(post=>{
+router.delete('/:id',async (req,res)=>{    
+    try {
+        let post = await Post.findByIdAndRemove(req.params.id);
         res.redirect('/admin/posts');
-    })
-    .catch(error=>{
-        console.log(error);
-    });
+    } catch (error) {
+        console.log(error)
+    }
 });
 
 module.exports = router;
