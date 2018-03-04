9fbc9e89b2829d97d5b43b4d7901b9aecf9329a2
Saad Zafar <31594740+saadie123@users.noreply.github.com>
2018-03-04 07:20:22 +0500

    Added posts on home page and single post page

--- routes/home/main.js
+++ routes/home/main.js
@@ -1,14 +1,17 @@
 const express = require('express');
 const router = express.Router();
 
+const Post = require('../../models/Post');
+
 router.all('/*',(req,res,next)=>{
     req.app.locals.layout = "home";
     next();
 });
 
 // Home page route
-router.get('/',(req,res)=>{
-    res.render('home/index');
+router.get('/',async (req,res)=>{
+    let posts = await Post.find();
+    res.render('home/index',{posts});
 });
 
 // About page route
@@ -19,11 +22,16 @@ router.get('/about',(req,res)=>{
 // Register page route
 router.get('/register',(req,res)=>{
     res.render('home/register');
-})
+});
 
 // Login page route
 router.get('/login',(req,res)=>{
     res.render('home/login');
-})
+});
+
+router.get('/post/:id',async (req,res)=>{
+    let post = await Post.findById(req.params.id);
+    res.render('home/post',{post});
+});
 
 module.exports = router;
