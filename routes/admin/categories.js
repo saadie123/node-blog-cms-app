5b80243e18f1f3b9f4e77c1e3af7c3b15dae6f2a
Saad Zafar <31594740+saadie123@users.noreply.github.com>
2018-03-04 11:37:06 +0500

    Added edit and delete category functaionality

--- routes/admin/categories.js
+++ routes/admin/categories.js
@@ -16,10 +16,20 @@ router.get('/',async (req,res)=>{
     }
 });
 
+router.get('/edit/:id',async (req,res)=>{
+    try {
+        let category = await Category.findById(req.params.id);
+        res.render('admin/categories/edit',{category});
+    } catch (error) {
+        console.log(error);
+    }
+})
+
 router.post('/create',async (req,res)=>{
     try {
         let errors = [];
         let checkCategory = await Category.findOne({name: req.body.name});
+        let allCategories = await Category.find();
         if(checkCategory){
             errors.push({error:'Category already exists!'});            
         }
@@ -27,18 +37,50 @@ router.post('/create',async (req,res)=>{
             errors.push({error:'Category name is required!'});
         }
         if(errors.length > 0){
-           return res.render('admin/categories/index',{errors});            
+           return res.render('admin/categories/index',{errors,categories:allCategories});            
         }
         let category = new Category({
             name: req.body.name
         });
         let newCategory = await category.save();
+        req.flash('success_message',`Category ${newCategory.name} was created successfully!`);                
         res.redirect('/admin/categories');
     } catch (error) {
         console.log(error);
     }
+});
+
+router.put('/edit/:id',async (req,res)=>{
+        try {
+            let errors = [];
+            let checkCategory = await Category.findOne({name: req.body.name});
+            if(checkCategory){
+                errors.push({error:'Category already exists!'});            
+            }
+            if(!req.body.name){
+                errors.push({error:'Category name is required!'});
+            }
+            if(errors.length > 0){
+                return res.render('admin/categories/edit',{errors,category:checkCategory});            
+            } else{
+                let body = req.body;
+                let updatedCategory = await Category.findByIdAndUpdate(req.params.id,{$set:body},{new:true});
+                req.flash('success_message',`Category ${updatedCategory.name} was updated successfully!`);                
+                res.redirect('/admin/categories');
+            }
+        } catch (error) {
+            console.log(error);
+        }
+});
+
+router.delete('/:id',async (req,res)=>{
+    try {
+        let category = await Category.findByIdAndRemove(req.params.id);
+        req.flash('success_message',`Category ${category.name} was deleted successfully!`);        
+        res.redirect('/admin/categories');        
+    } catch (error) {
+        console.log(error);
+    }
 })
-
-
 
 module.exports = router;
